5497706755bb643771a1d0871e6482c569109967
madridista18 <madridista18@naver.com>
2019-12-23 00:31:39 +0900

    styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import "./Movie.css";
 
-function Movie({id, year, title, summary, poster}) {
+function Movie({year, title, summary, poster}) {
   return (
-    <div>
-    <h5>{title}</h5>
-    <img src={poster}/>
+    <div class="movie">
+      <img src={poster} alt={title} title={title} />
+      <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+      </div>
     </div>
   );
 }
