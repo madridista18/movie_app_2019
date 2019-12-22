c2fc196761f074974ddbd5a8914834c2eeb1dc4e
madridista18 <madridista18@naver.com>
2019-12-22 18:16:27 +0900

    react map test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({name, picture}) {
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <img src={picture}/>
+    </div>
+  );
+}
+
+var foodILike = [
+  {
+    name: "kimchi", 
+    image: "http://m.geumchi.com/web/product/big/201608/20_shop1_616044.jpg"
+  },
+  {
+    name: "ramen", 
+    image: "http://file.mk.co.kr/meet/neds/2017/09/image_readtop_2017_587233_15042337473013492.jpg"
+  },
+  {
+    name: "samgiopsal", 
+    image: "https://pbs.twimg.com/media/EAo2Vv3UIAA3Dos.jpg"
+  },
+  {
+    name: "chadol", 
+    image: "http://static.hubzum.zumst.com/hubzum/2017/11/08/11/f33bc32ecc744f03b22a8669d64348de_780x0c.jpg"
+  }, 
+  {
+    name: "kimbap", 
+    image: "http://recipe1.ezmember.co.kr/cache/recipe/2015/04/04/0461907459756bc3a56472da407a1a9d1.jpg"
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
