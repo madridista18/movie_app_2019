4c5645e363878ff9bf8dccb071b2133acb122d9b
madridista18 <madridista18@naver.com>
2019-12-23 00:06:11 +0900

    exam: movie

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import axios from 'axios';
+import Movie from './Movie';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor call");
-  }
+class App extends React.Component { 
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies, isLoading: false});
   };
   componentDidMount() {
-    console.log("componentDidMount call");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
+    this.getMovies();
   }
   render() {
-    console.log("I'm rendering");
+    const {isLoading, movies} = this.state;
     return (
       <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading 
+          ? "Loading..." 
+          : movies.map(movie => (
+            <Movie 
+              key={movie.id}
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image}
+            />
+          ))}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Movie({id, year, title, summary, poster}) {
+  return (
+    <div>
+    <h5>{title}</h5>
+    <img src={poster}/>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: propTypes.number.isRequired, 
+  year: propTypes.number.isRequired,
+  title: propTypes.string.isRequired, 
+  summary: propTypes.string.isRequired, 
+  poster: propTypes.string.isRequired, 
+};
+
+export default Movie;
